8fec72e9661c482eea7a413f4989bfec4bf20f78
Alex Scarlett <alex.scarlett91@gmail.com>
2018-11-30 18:52:41 -0500

    full CRUD functionality with editing and deleting of posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -43,16 +43,31 @@ class PostsController extends Controller
    
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit')->with('post', $post);
     }
   
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        // Find Post and Update
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+
+        return redirect('/posts')->with('success', 'Post Updated');
     }
 
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+
+        return redirect('/posts')->with('success', 'Post Removed');
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,10 +2,17 @@
 
 @section('content')
     <a href="/posts" class="btn btn-info mb-4 text-white">Go Back</a>
-    <h1>{{ $post->title }}</h1>
+    <h1 class="mb-4">{{ $post->title }}</h1>
     <div>
-        {!! $post->body !!}
+        <h5 class="mt-2 mb-4">{!! $post->body !!}</h5>
     </div>
     <hr>
-    <small>Written on {{ $post->created_at }}</small>
+    <p>Written on {{ $post->created_at }}</p>
+    <hr>
+    <a href="/posts/{{ $post->id }}/edit" class="btn btn-success text-white">Edit</a>
+
+    {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' =>'POST', 'class' => 'float-right']) !!}
+        {!! Form::hidden('_method', 'DELETE') !!}
+        {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
 @endsection
