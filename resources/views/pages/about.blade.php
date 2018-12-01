e2756b36101fa41eae9af313bd1ce07d2058b973
Alex Scarlett <alex.scarlett91@gmail.com>
2018-12-01 00:13:10 -0500

    Added a few filler paragraphs to About page, deleted a few unnecessary files

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,5 +2,13 @@
 
 @section('content')
     <h1>{{ $title }}</h1>
-    <p>This is the About Page</p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas laoreet, enim sit amet feugiat vulputate, purus neque tristique erat, a luctus odio nisi ut odio. Aliquam dictum malesuada tempus. Morbi eu turpis ut velit tincidunt rhoncus eu at urna. Nullam lobortis est in quam tristique, in tempus orci venenatis. Phasellus quis elit sollicitudin, commodo ligula cursus, imperdiet purus. Vestibulum laoreet purus ac elit iaculis gravida. Proin sed tempus sem. Nulla nulla metus, porta et vestibulum vel, dictum sit amet nisi. Sed velit dolor, pharetra nec justo non, dapibus vulputate lacus. Cras elit massa, tempor sit amet elementum vitae, hendrerit quis nunc.
+    </p>
+    <p>
+        Integer laoreet ante at sapien tempor tristique. Proin accumsan rhoncus laoreet. Donec euismod ipsum ipsum, a cursus odio gravida id. Aliquam ornare sagittis dui, vel venenatis dolor scelerisque vitae. Aliquam accumsan felis quis vulputate porttitor. Etiam ut iaculis est. Curabitur eu arcu a leo sodales congue. Curabitur lacinia urna ut vestibulum ultricies. Curabitur in molestie velit, ac efficitur nunc.
+    </p>
+    <p>
+        Nullam id ullamcorper mi, eget volutpat ex. Vivamus nec tellus dolor. Curabitur vel massa viverra, laoreet ante at, vulputate risus. Maecenas faucibus elit eget lectus consequat porttitor. Quisque quis pulvinar justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nunc nulla, aliquam sit amet libero ornare, tempus interdum augue. Aenean pellentesque libero justo, sollicitudin fermentum lectus tincidunt ut. Sed a dui id massa volutpat venenatis a sit amet urna. Fusce mollis, mauris a placerat ornare, tortor lacus auctor nisl, vel sodales velit diam vel felis. In vel lectus laoreet, imperdiet justo sed, feugiat mi. Mauris tortor risus, euismod in mollis id, commodo non ante. Integer condimentum enim eget nisi maximus tincidunt. Nam vel pellentesque enim. Sed sit amet dui vestibulum, finibus mi id, iaculis nulla.
+    </p>
 @endsection
