0d7f3b69aed8ed11b2643b6da644be4586432da3
Alex Scarlett <alex.scarlett91@gmail.com>
2018-11-30 19:33:39 -0500

    user_id added to posts table, full login and register functionality, user auth

---
+++ database/migrations/2018_12_01_002006_add_user_id_to_posts.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdToPosts extends Migration
+{
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->integer('user_id');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+}
